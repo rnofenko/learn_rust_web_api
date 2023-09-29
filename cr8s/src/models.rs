f1f5ae14c0f1239843f2ea9bdc12e2d00ff4c05d
unknown <rnofenko@gmail.com>
2023-09-28 20:07:12 -0700

    models

---
+++ cr8s/src/models.rs
@@ -0,0 +1,39 @@
+use chrono::NaiveDateTime;
+use diesel::{prelude::{Insertable, Queryable}, query_builder::AsChangeset};
+use crate::schema::*;
+
+#[derive(Queryable, AsChangeset)]
+pub struct Rustacean {
+    pub id: i32,
+    pub name: String,
+    pub email: String,
+    pub created_at: NaiveDateTime,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name=rustaceans)]
+pub struct NewRustacean {
+    pub name: String,
+    pub email: String,
+}
+
+#[derive(Queryable, AsChangeset)]
+pub struct Crate {
+    pub id: i32,
+    pub rustacean_id: i32,
+    pub code: String,
+    pub name: String,
+    pub version: String,
+    pub description: Option<String>,
+    pub created_at: NaiveDateTime,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name=crates)]
+pub struct NewCrate {
+    pub rustacean_id: i32,
+    pub code: String,
+    pub name: String,
+    pub version: String,
+    pub description: Option<String>,
+}
