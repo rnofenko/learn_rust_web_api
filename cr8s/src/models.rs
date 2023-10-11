8d60cce07b7f9bc5f7da6a4b68b0e19bd55cc35d
rnofenko <rnofenko@gmail.com>
2023-10-10 23:19:28 -0700

    #9

--- cr8s/src/models.rs
+++ cr8s/src/models.rs
@@ -40,12 +40,14 @@ pub struct NewPerson {
 
 #[derive(Queryable, AsChangeset, Deserialize, Serialize)]
 pub struct Crate {
+    #[serde(skip_deserializing)]
     pub id: i32,
     pub rustacean_id: i32,
     pub code: String,
     pub name: String,
     pub version: String,
     pub description: Option<String>,
+    #[serde(skip_deserializing)]
     pub created_at: NaiveDateTime,
 }
 
--- cr8s/tests/rustaceans.rs
+++ cr8s/tests/rustaceans.rs
@@ -1,21 +1,13 @@
 use reqwest::{blocking::Client, StatusCode};
-use rocket::serde::json::{serde_json::json, Value};
+use serde_json::{json, Value};
 
-fn create_test_rustacean(client: &Client) -> Value {
-    let create_response = client
-        .post("http://127.0.0.1:8000/rustaceans")
-        .json(&json!({"name": "test_name", "email": "test@mail.com"}))
-        .send()
-        .unwrap();
-    assert_eq!(create_response.status(), StatusCode::CREATED);
-    create_response.json().unwrap()
-}
+pub mod common;
 
 #[test]
 fn test_get_rustaceans() {
     let client = Client::new();
     let response = client
-        .get("http://127.0.0.1:8000/rustaceans")
+        .get(format!("{}/rustaceans", common::APP_HOST))
         .send()
         .unwrap();
     assert_eq!(response.status(), StatusCode::OK);
@@ -25,7 +17,7 @@ fn test_get_rustaceans() {
 fn test_create_rustacean() {
     let client = Client::new();
     let response = client
-        .post("http://127.0.0.1:8000/rustaceans")
+        .post(format!("{}/rustaceans", common::APP_HOST))
         .json(&json!({"name": "test name1", "email": "test@mail.com"}))
         .send()
         .unwrap();
@@ -40,16 +32,16 @@ fn test_create_rustacean() {
 
 #[test]
 fn test_view_rustacean() {
-    let name = "test_name";
-    let email = "test@mail.com";
+    let name = "Foo bar";
+    let email = "foo@bar.com";
     let client = Client::new();
 
-    let rustacean: Value = create_test_rustacean(&client);
+    let rustacean: Value = common::create_test_rustacean(&client);
     let id = &rustacean["id"];
     let created_at = &rustacean["created_at"];
 
     let view_response = client
-        .get(format!("http://127.0.0.1:8000/rustaceans/{}", id))
+        .get(format!("{}/rustaceans/{}", common::APP_HOST, id))
         .send()
         .unwrap();
     assert_eq!(view_response.status(), StatusCode::OK);
@@ -66,12 +58,12 @@ fn test_update_rustacean() {
     let name_new = "test_name_3";
     let email = "test@mail.com";
     let client = Client::new();
-    let rustacean: Value = create_test_rustacean(&client);
+    let rustacean: Value = common::create_test_rustacean(&client);
     let id = &rustacean["id"];
     let created_at = &rustacean["created_at"];
 
     let update_response = client
-        .put(format!("http://127.0.0.1:8000/rustaceans/{}", id))
+        .put(format!("{}/rustaceans/{}", common::APP_HOST, id))
         .json(&json!({"name": name_new, "email": email}))
         .send()
         .unwrap();
@@ -87,11 +79,11 @@ fn test_update_rustacean() {
 #[test]
 fn test_delete_rustacean() {
     let client = Client::new();
-    let rustacean: Value = create_test_rustacean(&client);
+    let rustacean: Value = common::create_test_rustacean(&client);
     let id = &rustacean["id"];
 
     let response = client
-        .delete(format!("http://127.0.0.1:8000/rustaceans/{}", id))
+        .delete(format!("{}/rustaceans/{}", common::APP_HOST, id))
         .send()
         .unwrap();
     assert_eq!(response.status(), StatusCode::NO_CONTENT);
